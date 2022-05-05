38a801203e60407c90ab3349f1cc9648e6f0c843
Kunal78github <104549034+Kunal78github@users.noreply.github.com>
2022-05-05 13:54:22 +0530

    Add files via upload

---
+++ ArrayEx3.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+//to print an array using to string method
+public class ArrayEx3 {
+    public static void main(String[] args) {
+        int a[] = {2,32,32,6,2,6,6};
+        System.out.println(Arrays.toString(a));
+    }
+}
---
+++ ArrayEx6.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//converting array to arraylist
+public class ArrayEx6 {
+    public static void main(String[] args) {
+        //create an array
+        String a[] = {"Java, Python, ADS"};
+        System.out.println("Array elements" + Arrays.toString(a));
+        //convert to arraylist
+        List Languages = new ArrayList<>(Arrays.asList(a));
+        System.out.println("List : " + Languages);
+
+    }
+}
